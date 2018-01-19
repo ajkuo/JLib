1b74092dfaaaa2ae7ab916eefc5a28163367c663
Jie Kuo <anjiekuo@gmail.com>
2018-01-19 16:44:16 +0800

    Make it better

--- run.py
+++ run.py
@@ -2,10 +2,10 @@
 from JLib import JTime
 from JLib.JConfig import Rsp
 
-ts = JTime.GetTimestamp()
-res = Rsp.Loads(ts) 
-print(ts)
+res = Rsp.Loads(JTime.GetTimestamp()) 
+
 if not res.error:
     print(res.message)
+    
 else:
     print("{} [DEBUG] {}".format(res.message, res.debug))
