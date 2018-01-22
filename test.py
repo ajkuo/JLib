c06535436ec092d8d8a117efec719606fd8cac59
Jie Kuo <anjiekuo@gmail.com>
2018-01-22 10:07:45 +0800

    Make it better.

--- test.py
+++
@@ -1,9 +0,0 @@
-from JLib import JTime
-from datetime import datetime
-
-
-dt = datetime(1970, 1, 1, 17, 2, 43, 123)
-
-print(datetime.strftime(dt, "%Y-%m-%d %H:%M:%S.%f"))
-
-#print(JTime.TimestampToDatetime(1516406644.478761))
